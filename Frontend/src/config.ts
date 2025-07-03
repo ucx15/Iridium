22db7ac14df5b3b91031b4c8de3d625a35808a95
ucx15 <83822686+ucx15@users.noreply.github.com>
2025-07-04 00:36:14 +0530

    Rename : config.ts logs

--- Frontend/src/config.ts
+++ Frontend/src/config.ts
@@ -7,6 +7,6 @@
 const environment = import.meta.env;
 
 const BACKEND_URI = environment.VITE_BACKEND_URI || "http://localhost:5000/api";
-console.log("Environment variables:", environment.VITE_BACKEND_URI);
+console.log("Backend URI:", environment.VITE_BACKEND_URI);
 
 export default BACKEND_URI;
